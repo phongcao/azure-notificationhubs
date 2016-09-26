97bdded8bb26cd579f9d81c70e481e3cd06b3440
Phong Cao <phong.caothai@live.com>
2016-09-26 16:08:33 -0400

    Created iOS target.

---
+++ iOS/WindowsAzureMessaging/WindowsAzureMessagingFramework/WindowsAzureMessagingFramework.h
@@ -0,0 +1,19 @@
+//
+//  WindowsAzureMessagingFramework.h
+//  WindowsAzureMessagingFramework
+//
+//  Created by Phong Cao on 9/26/16.
+//  Copyright © 2016 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WindowsAzureMessagingFramework.
+FOUNDATION_EXPORT double WindowsAzureMessagingFrameworkVersionNumber;
+
+//! Project version string for WindowsAzureMessagingFramework.
+FOUNDATION_EXPORT const unsigned char WindowsAzureMessagingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WindowsAzureMessagingFramework/PublicHeader.h>
+
+
